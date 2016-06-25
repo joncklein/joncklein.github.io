e70a46a21289b79c8f7197909ccb655b307817ba
joncklein <joncklein@gmail.com>
2016-06-25 21:31:47 +0100

    Add files via upload
    functional interface, before assigning functions to buttons

--- dealerinterface.py
+++ dealerinterface.py
@@ -14,36 +14,34 @@ class Application(Frame):
         
     def create_widgets(self):
         #create buttons
-        self.title = Label(self, text = "Rent a car:    ")
+        self.title = Label(self, text = "Select car type:    ")
         self.title.grid(row = 3, column = 1, columnspan = 2, sticky = W)
         #instruction text
-        self.instruction1 = Label(self, text = "Would you like to rent a car:   ")
+        self.instruction1 = Label(self, text = "Would you like to rent or return:   ")
         self.instruction1.grid(row = 0, column = 1, columnspan = 2, sticky = W)
         
-        self.instruction2 = Label(self, text = "Enter second number: ")
-        self.instruction2.grid(row = 2, column = 1, columnspan = 2, sticky = W)
         #fields to enter numbers
-        self.yes_button = Button(self, text ="yes") #command = self.petrol#)
-        self.yes_button.grid(row = 1, column = 1, columnspan = 1, sticky = W)
+        self.rent_button = Radiobutton(self, text ="rent", value = 1) #command = self.petrol#)
+        self.rent_button.grid(row = 1, column = 1, columnspan = 1, sticky = W)
 		
-        self.no_button = Button(self, text ="no") #command = self.petrol#)
-        self.no_button.grid(row = 1, column = 2, sticky = W)
+        self.return_button = Radiobutton(self, text ="return", value = 2) #command = self.petrol#)
+        self.return_button.grid(row = 1, column = 2, sticky = W)
         
-        # self.text = Text(self, width = 35, height = 5, wrap = WORD)
-        # self.text.grid(row = 13, column = 1, sticky = W)
         #buttons for functions
-        self.petrol_button = Button(self, text ="petrol",) #command = self.petrol#)
+        self.petrol_button = Checkbutton(self, text ="petrol",) #command = self.petrol#)
         self.petrol_button.grid(row = 4, column = 1, sticky = W)
         
-        self.diesel_button = Button(self, text ="diesel",) #command = self.diesel#)
+        self.diesel_button = Checkbutton(self, text ="diesel",) #command = self.diesel#)
         self.diesel_button.grid(row = 4, column = 2)
         
-        self.electric_button = Button(self, text ="electric",) #command = self.electric#)
+        self.electric_button = Checkbutton(self, text ="electric",) #command = self.electric#)
         self.electric_button.grid(row = 6, column = 1, sticky = NW)
         
-        self.hybrid_button = Button(self, text ="hybrid",) #command = self.hybrid#)
-        self.hybrid_button.grid(row = 7, column = 1, sticky = NW)
+        self.hybrid_button = Checkbutton(self, text ="hybrid",) #command = self.hybrid#)
+        self.hybrid_button.grid(row = 6, column = 2, sticky = W)
   
+        self.text = Text(self, width = 20, height = 5, wrap = WORD)
+        self.text.grid(row = 7, column = 1, sticky = W)
     
 
 #set up GUI
